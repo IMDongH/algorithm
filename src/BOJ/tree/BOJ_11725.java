6192409124d22f1cbf17b0f50d88e2bf709298b5
임동혁 <idh1007@naver.com>
2023-03-10 23:27:49 +0900

    [ADD] tree

--- src/BOJ/tree/BOJ_11725.java
+++ src/BOJ/tree/BOJ_11725.java
@@ -4,28 +4,62 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
 
 public class BOJ_11725 {
 
-    static ArrayList<ArrayList<Integer>> arr;
+
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
 
-        arr = new ArrayList<>();
-
-        for(int i = 0 ; i<=n; i++){
-            arr.add(new ArrayList<Integer>());
-        }
-        for(int i=0; i<n; i++){
-            char[] str = br.readLine().replaceAll(" ", "").toCharArray();
-
-            System.out.println(str[0]);
-            arr.get(Integer.parseInt(String.valueOf(str[0]))).add((int) str[1]);
-
+        // 인접 리스트
+        ArrayList< ArrayList<Integer> > list = new ArrayList< ArrayList<Integer> >();
+        int[] parents = new int[n+1];
+        int i;
+        for(i=0; i<=n+1; i++) {
+            list.add(new ArrayList<Integer>());
         }
 
+        // 연결 설정
+        int a, b;
+        for(i=1; i<n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            list.get(a).add(b);
+            list.get(b).add(a);
+        }
+
+        // bfs
+        int start = 1;
+        bfs(start, list, parents, n);
+
+        // 결과 출력
+        printParents(parents);
+    }
+
+    private static void bfs(int start, ArrayList<ArrayList<Integer>> list, int[] parents, int n) {
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+        queue.offer(start);
+        parents[start] = 1;
+
+        while(!queue.isEmpty()) {
+            int parent = queue.poll();
+
+            for(int item : list.get(parent)) {
+                if(parents[item] == 0) {
+                    parents[item] = parent;
+                    queue.offer(item);
+                }
+
+            }// end for
+        }
+    }
+
+    private static void printParents(int[] parents) {
+        int i;
+        for(i=2; i<parents.length; i++) System.out.println(parents[i]);
     }
 }
