c733fef1aa5f99769a4175711d4360bf061d6b21
임동혁 <idh1007@naver.com>
2023-04-06 10:34:09 +0900

    [ADD]

--- src/BOJ/graph/BOJ_1987.java
+++ src/BOJ/graph/BOJ_1987.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+
 public class BOJ_1987 {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
